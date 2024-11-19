4e347aef5b754a90b2015e17619d47de1fd68d1d
Undid-Iridium <24619207+Undid-Iridium@users.noreply.github.com>
2024-11-18 21:12:13 -0500

    Option A

--- EXILED/Exiled.Events/Patches/Events/Server/Unban.cs
+++ EXILED/Exiled.Events/Patches/Events/Server/Unban.cs
@@ -5,36 +5,37 @@
 // </copyright>
 // -----------------------------------------------------------------------
 
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using Exiled.API.Features.Pools;
+using Exiled.Events.Attributes;
+using Exiled.Events.EventArgs.Server;
+using HarmonyLib;
+
 namespace Exiled.Events.Patches.Events.Server
 {
-    using System.Collections.Generic;
-    using System.Reflection.Emit;
-
-    using Exiled.API.Features.Pools;
-    using Exiled.Events.Attributes;
-    using Exiled.Events.EventArgs.Server;
-    using HarmonyLib;
-
-    using static HarmonyLib.AccessTools;
+    using static AccessTools;
 
     /// <summary>
-    /// Patches <see cref="BanHandler.RemoveBan"/>
-    /// to add <see cref="Handlers.Server.Unbanning"/> and <see cref="Handlers.Server.Unbanned"/> events.
+    ///     Patches <see cref="BanHandler.RemoveBan" />
+    ///     to add <see cref="Handlers.Server.Unbanning" /> and <see cref="Handlers.Server.Unbanned" /> events.
     /// </summary>
     [HarmonyPatch(typeof(BanHandler), nameof(BanHandler.RemoveBan))]
     [EventPatch(typeof(Handlers.Server), nameof(Handlers.Server.Unbanning))]
     [EventPatch(typeof(Handlers.Server), nameof(Handlers.Server.Unbanned))]
     internal class Unban
     {
-        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+        [HarmonyTranspiler]
+        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions,
+            ILGenerator generator)
         {
-            List<CodeInstruction> newInstructions = ListPool<CodeInstruction>.Pool.Get(instructions);
+            var newInstructions = ListPool<CodeInstruction>.Pool.Get(instructions);
 
-            LocalBuilder ev = generator.DeclareLocal(typeof(UnbanningEventArgs));
+            var ev = generator.DeclareLocal(typeof(UnbanningEventArgs));
 
-            Label continueLabel = generator.DefineLabel();
+            var continueLabel = generator.DefineLabel();
 
-            newInstructions.InsertRange(0, new CodeInstruction[]
+            newInstructions.InsertRange(0, new[]
             {
                 // id
                 new(OpCodes.Ldarg_0),
@@ -64,7 +65,7 @@ namespace Exiled.Events.Patches.Events.Server
                 // id = ev.TargetId;
                 new CodeInstruction(OpCodes.Ldloc_S, ev.LocalIndex).WithLabels(continueLabel),
                 new(OpCodes.Callvirt, PropertyGetter(typeof(UnbanningEventArgs), nameof(UnbanningEventArgs.TargetId))),
-                new(OpCodes.Starg_S, 0),
+                new(OpCodes.Starg_S, 0)
             });
 
             newInstructions.InsertRange(newInstructions.Count - 1, new CodeInstruction[]
@@ -79,11 +80,70 @@ namespace Exiled.Events.Patches.Events.Server
                 new(OpCodes.Newobj, GetDeclaredConstructors(typeof(UnbannedEventArgs))[0]),
 
                 // Handlers.Server.OnUnbanned(ev2);
-                new(OpCodes.Call, Method(typeof(Handlers.Server), nameof(Handlers.Server.OnUnbanned))),
+                new(OpCodes.Call, Method(typeof(Handlers.Server), nameof(Handlers.Server.OnUnbanned)))
             });
 
-            for (int z = 0; z < newInstructions.Count; z++)
+            for (var z = 0; z < newInstructions.Count; z++)
+            {
                 yield return newInstructions[z];
+            }
+
+            ListPool<CodeInstruction>.Pool.Return(newInstructions);
+        }
+
+        [HarmonyTranspiler]
+        private static IEnumerable<CodeInstruction> BanHandlerTranspiler(
+            IEnumerable<CodeInstruction> instructions,
+            ILGenerator generator)
+        {
+            var newInstructions = ListPool<CodeInstruction>.Pool.Get(instructions);
+
+            var ev = generator.DeclareLocal(typeof(UnbanningEventArgs));
+
+            var continueLabel = generator.DefineLabel();
+
+            const int offset = 2;
+            var index = newInstructions.FindIndex(instruction =>
+                instruction.Calls(Method(typeof(BanHandler), nameof(BanHandler.CheckExpiration)))) + offset;
+
+
+            var addToUnbannedListInstruction = newInstructions[index];
+            newInstructions.InsertRange(index, new[]
+            {
+                // id
+                new CodeInstruction(OpCodes.Ldarg, 4).MoveLabelsFrom(addToUnbannedListInstruction),
+
+                // type
+                new(OpCodes.Ldarg_0),
+
+                // true
+                new(OpCodes.Ldc_I4_1),
+
+                // UnbanningEventArgs ev = new(string, BanHandler.BanType, true);
+                new(OpCodes.Newobj, GetDeclaredConstructors(typeof(UnbanningEventArgs))[0]),
+
+                new(OpCodes.Dup),
+                new(OpCodes.Stloc_S, ev.LocalIndex),
+
+                // Handlers.Server.OnUnbanning(ev);
+                new(OpCodes.Call, Method(typeof(Handlers.Server), nameof(Handlers.Server.OnUnbanning))),
+
+                // if (!ev.IsAllowed)
+                //    return;
+                new(OpCodes.Callvirt, PropertyGetter(typeof(UnbanningEventArgs), nameof(UnbanningEventArgs.IsAllowed))),
+                new(OpCodes.Brtrue_S, continueLabel),
+
+                new(OpCodes.Ret)
+            });
+
+            // Add label to ldloc.1
+            addToUnbannedListInstruction.WithLabels(continueLabel);
+
+
+            for (var z = 0; z < newInstructions.Count; z++)
+            {
+                yield return newInstructions[z];
+            }
 
             ListPool<CodeInstruction>.Pool.Return(newInstructions);
         }
